92897b1d2564b1029f6c4a7385b3b3dbd71bb251
寫詩羊 <Habuche@gmail.com>
2018-08-31 00:18:59 +0800

    chore(website): append ga and logrocket

--- website/src/index.js
+++ website/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 import './index.css';
 import App from './App';
+
+LogRocket.init('sexyoung/o18n');
+setupLogRocketReact(LogRocket);
 
 ReactDOM.render(<App />, document.getElementById('root'));
